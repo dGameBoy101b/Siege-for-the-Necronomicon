c65000183ab014506ff254f92736a6c5deaa104b
dGameBoy101b <rhysmader@gmail.com>
2021-03-28 01:17:41 +0800

    Create Base Projectile Classes
    Created abstract base class for all projectiles.
    Created abstract base class for all magical projectiles.
    Created abstract base class for all physical projectiles.
    Added separate collision layers for magical and physical projectiles.
    Created simple magical projectile.
    Created simple physical projectile.

---
+++ Assets/Objects/Projectiles/MagicalProjectileBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author Rhys Mader 33705134
+ * @date 27/03/2021
+ * The abstract base class for all magical projectiles that must be blocked by the player.
+ */
+public abstract class MagicalProjectileBase : ProjectileBase
+{
+	/**
+	 * The name of the collision layer for all magical projectiles.
+	 */
+	public static string COL_LAYER {get;} = "Magical Projectile";
+	
+	/**
+	 * Set the collision layer of this magical projectile.
+	 */
+	protected override void setCollisionLayer()
+	{
+		this.gameObject.layer = LayerMask.NameToLayer(MagicalProjectileBase.COL_LAYER);
+	}
+}
---
+++ Assets/Objects/Projectiles/Simple Magical/SimpleMagicalProjectile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author Rhys Mader 33705134
+ * @date 27/03/2021
+ * A simple magical projectile that moves in a straight line towards a target position at a constant velocity.
+ */
+public sealed class SimpleMagicalProjectile : MagicalProjectileBase
+{
+	/**
+	 * Construct a new simple magical projectile with the given attributes.
+	 * @param speed The float speed in units per second at which this projectile moves forward.
+	 * @param target The global position this simple magic protectile is aimed at.
+	 */
+	public SimpleMagicalProjectile(float speed, Vector3 target)
+	{
+		this.Speed = speed;
+		this.Target = target;
+	}
+	
+	/**
+	 * The global position this simple magical projectile willl fly towards.
+	 */
+	public Vector3 Target {get; private set;}
+	
+	/**
+	 * The speed in units per second at which this simple magical projectile moves forward.
+	 */
+	public float Speed {get; private set;}
+	
+	/**
+	 * Rotate this simple magical projectile to face the target.
+	 */
+	private void faceTarget()
+	{
+		this.transform.rotation = Quaternion.LookRotation(this.Target - this.transform.position);
+	}
+	
+	/**
+	 * Move this simple magical projectile forwards.
+	 * @param t The float number of seconds since this function was called.
+	 */
+	private void moveForward(float t)
+	{
+		this.transform.position += Vector3.forward * this.Speed * t;
+	}
+	
+	private void Start()
+	{
+		this.faceTarget();
+	}
+	
+	private void Update()
+	{
+		this.moveForward(Time.deltaTime);
+	}
+}
