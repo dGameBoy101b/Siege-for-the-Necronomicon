9b5622005a6531b932f6f0615f0bc4e8ffc409d5
dGameBoy101b <rhysmader@gmail.com>
2021-04-08 13:34:46 +0800

    Basic Sword Slashing
    Created abstract base class for equipment that deals with handedness.
    Created a basic sword slash prefab.
    Created the sword slash script to move it forward.
    Added placeholder model and material for sword slash.
    Added collision layer for sword slashes.
    Created concrete class for the sword to create the sword slashes with a trigger press.
    Created prefab for sword.
    Added placeholder model and material for sword.
    Added collision layer for the player's sword.
    Created VR avatar prefab.
    Added Kai's OVRController to the prefab.
    Added and configured sword prefab.
    Added collision layer for the player.

---
+++ Assets/Objects/Player/EquipmentBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EquipmentBase : MonoBehaviour
+{
+	[Header("Player")]
+	
+	[SerializeField()]
+	[Tooltip("The player's center.")]
+	public Transform PLAYER;
+	
+	[SerializeField()]
+	[Tooltip("The object to attach this object to when the left hand is dominant.")]
+	public Transform LEFT_HAND;
+	
+	[SerializeField()]
+	[Tooltip("The object to attach this object to when the right hand is dominant.")]
+	public Transform RIGHT_HAND;
+	
+	[SerializeField()]
+	[Tooltip("The object to attach this object to when there is no dominant hand.")]
+	public Transform DEFAULT_HAND;
+	
+	/**
+	 * Update the hand this object is attached to.
+	 */
+	public void updateHandedness()
+	{
+		Transform hand;
+		switch (OVRInput.GetDominantHand())
+		{
+			case OVRInput.Handedness.LeftHanded:
+				hand = this.LEFT_HAND;
+				break;
+			case OVRInput.Handedness.RightHanded:
+				hand = this.RIGHT_HAND;
+				break;
+			default:
+				hand = this.DEFAULT_HAND;
+				break;
+		}
+		this.transform.SetParent(hand, false);
+	}
+	
+	protected void Start()
+	{
+		this.updateHandedness();
+	}
+}
