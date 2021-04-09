373883ba0d8fdbe1ec04a888ba8cf7190852b64a
Connor-Burnside <49231255+Connor-Burnside@users.noreply.github.com>
2021-04-09 13:46:29 +0800

    Save-Load System
    added basic save load system that can store the serializable data types in unity as a custom binary file

---
+++ Assets/Objects/SaveLoad/SaveLoad.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+//accesses the binary formatter in unity
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveLoad
+{
+    //save path, persistentDataPath gives a path on any computer to a data file
+    private static string path = Application.persistentDataPath + "/player.u";
+    
+    public static void SavePlayer(TestPlayer player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+    
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        //pass the player into the playerdata constructor
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+
+    public static PlayerData LoadPLayer()
+    {
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+    
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }else
+        {
+            Debug.LogError("Save file nto found in " + path);
+            return null;
+        }
+    }
+}
