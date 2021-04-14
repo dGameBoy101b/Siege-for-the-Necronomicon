1f44052ec3141aee6719eb911cb28626f866511c
kaiswe <kaisweeting13@gmail.com>
2021-04-14 18:12:08 +0800

    UI timer doxygen documentation

--- Assets/Objects/Player/UI/Scripts/UITimer.cs
+++ Assets/Objects/Player/UI/Scripts/UITimer.cs
@@ -11,21 +11,41 @@ public class UITimer : MonoBehaviour
 
     private void Start()
     {
-        // Starts the timer automatically
+        /**
+         * Set timer to true on application start
+         * automatically starts if true
+         */
+
         timerIsRunning = true;
     }
 
     void Update()
     {
+        /**
+         * Check if timer is set to true
+         * only execute if-statement if timer is set to true
+         */
+
         if (timerIsRunning)
         {
+            /** 
+             * If true and greater than 0 start counting down from specified seconds
+             */
             if (timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
-                DisplayTime(timeRemaining);
+                /**
+                 * @return timeRemaining To DisplayTimer to show countdown on UI
+                 */
+                DisplayTimer(timeRemaining);
             }
             else
             {
+                /**
+                 * If timer hits zero display game over
+                 * set timerIsRunning to false and lock timer 
+                 */
+                
                 Debug.Log("Game Over!");
                 timeRemaining = 0;
                 timerIsRunning = false;
@@ -33,13 +53,23 @@ public class UITimer : MonoBehaviour
         }
     }
 
-    void DisplayTime(float timeDisplay)
+    /** 
+     *@param showTime The float timeRemaining for seconds specified
+     */
+    void DisplayTimer(float showTime)
     {
-        timeDisplay += 1;
 
-        float minutes = Mathf.FloorToInt(timeDisplay / 60); 
-        float seconds = Mathf.FloorToInt(timeDisplay % 60);
+        showTime += 1;
 
+        /**
+         * calculate the time remaining from specified seconds
+         */
+        float minutes = Mathf.FloorToInt(showTime / 60); 
+        float seconds = Mathf.FloorToInt(showTime % 60);
+        
+        /**
+         * display remaining minutes and seconds values in text
+         */
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
