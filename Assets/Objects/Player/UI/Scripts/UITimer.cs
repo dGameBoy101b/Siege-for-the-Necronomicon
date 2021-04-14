6a76355e0800e5ef9859877b73fb0199c92ffff0
kaiswe <kaisweeting13@gmail.com>
2021-04-14 15:58:34 +0800

    UI prefab adjustments
    removed duplicate prefab, improved timer

---
+++ Assets/Objects/Player/UI/Scripts/UITimer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Game Over!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeDisplay)
+    {
+        timeDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
