fce24d2c117e28351988ae4b8aaed14779cc6c6c
dGameBoy101b <rhysmader@gmail.com>
2021-04-15 19:24:03 +0800

    Fix Corruption
    Fixed multiple issues resulting from the merging process.
    Added Player prefab to contain both the VR and PC avatars.

---
+++ Assets/Objects/Player/PlatformSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author Rhys Mader 33705134
+ * @date 15/04/2021
+ * A class to conditionally enable game objects based on the runtime platform.
+ */
+public class PlatformSelector : MonoBehaviour
+{
+	[SerializeField()]
+	[Tooltip("The platforms this object should be enabled for.")]
+	public List<RuntimePlatform> PLATFORMS;
+	
+	/**
+	 * Check if this game object should be active.
+	 */
+	private void checkPlatform()
+	{
+		this.gameObject.SetActive(this.PLATFORMS.Contains(Application.platform));
+	}
+	
+	private void Awake()
+	{
+		this.checkPlatform();
+	}
+}
