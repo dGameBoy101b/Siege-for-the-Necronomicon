414792e504566a37bc1b67c5ecd2af3f19d8a331
kaiswe <kaisweeting13@gmail.com>
2021-04-17 00:18:34 +0800

    Updated health UI component
    tracks damage taken on hit
    UI adjustments

---
+++ Assets/Objects/Player/UI/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public int maxHealth;
+    public Text currentHealthLabel;
+    public int currentHealth;
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateGUI();
+        
+    }
+
+    void UpdateGUI()
+    {
+        currentHealthLabel.text = currentHealth.ToString();
+         if ( currentHealth == 0)
+        {
+            Debug.Log("Game Over!");
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Projectile"){
+            currentHealth -= 1;
+            Debug.Log("You have been hit!");
+        }
+    }
+
+    public void UpdateHealth(int damage)
+    {
+        currentHealth += damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateGUI();
+    }
+}
