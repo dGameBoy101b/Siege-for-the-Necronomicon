bfe2d19fb164816cebc536c43e1d345ae24ca684
Connor-Burnside <49231255+Connor-Burnside@users.noreply.github.com>
2021-04-17 18:38:40 +0800

    Level 1 setup
    Placing objects in level and testing with minor bug fixes

---
+++ Assets/Objects/Player/Gauntlet/Gauntlet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+*@author Connor Burnside 33394927   
+*@date 8/04/2021
+* a gaunlet object that collides with magical projectiles, destroying them and adding to the score, inherits from the EquipmentBase
+*/
+public class Gauntlet : EquipmentBase
+{
+    //destroy the projectile on collision
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.layer == 8)  
+        {
+            Destroy(other.gameObject);
+            Debug.Log("blocked magic projectile");
+        }  
+    }
+}
