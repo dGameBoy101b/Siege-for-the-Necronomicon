8f05e85b10bcf8954aac685fca7911e47befbd7f
dGameBoy101b <rhysmader@gmail.com>
2021-05-12 00:55:27 +0800

    Added Option Store and Slider
    Added a persistent singleton to store, save, and load options.
    Added a slider prefab to interface with the option store.
    Still requires testing.

---
+++ Assets/Objects/Options/OptionStore.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/**
+ * @author Rhys Mader 33705134
+ * @date 11/05/2021
+ * A class for storing and serialising options.
+ */
+public sealed class OptionStore : MonoBehaviour
+{
+	/**
+	 * An exception class to indicate a name clash in the options dictionary.
+	 */
+	public class OptionAlreadyExistsException : Exception
+	{
+		public OptionAlreadyExistsException() : base() {}
+		public OptionAlreadyExistsException(string msg) : base(msg) {}
+		public OptionAlreadyExistsException(string msg, Exception inner) : base(msg, inner) {}
+	}
+	
+	/**
+	 * An exception class to indicate a name mismatch with the options dictionary.
+	 */
+	public class OptionDoesNotExistException : Exception
+	{
+		public OptionDoesNotExistException() : base() {}
+		public OptionDoesNotExistException(string msg) : base(msg) {}
+		public OptionDoesNotExistException(string msg, Exception inner) : base(msg, inner) {}
+	}
+	
+	[SerializeField()]
+	[Tooltip("The string name of the file to save these options to or load from.")]
+	public string FILENAME;
+	
+	/**
+	 * A singleton instance of this option store.
+	 */
+	public static OptionStore Instance {get; private set;} = null;
+	
+	/**
+	 * A dictionary of all named options.
+	 */
+	private Dictionary<string, object> options = new Dictionary<string, object>();
+	
+	/**
+	 * Add a named option to this option store.
+	 * @param name The string name of the new option.
+	 * @param val The initial value of the new option.
+	 * @throws OptionStore.OptionAlreadyExistsException An option with the given name already exists.
+	 */
+	public void addOption(string name, object val)
+	{
+		if (this.hasOption(name))
+		{
+			throw new OptionStore.OptionAlreadyExistsException("Option \"" + name + "\" already exists!");
+		}
+		this.options.Add(name, val);
+	}
+	
+	/**
+	 * Set the value of the given named option to the given value.
+	 * @param name The string name of the existing option to target.
+	 * @param val The new value to set the target option to.
+	 * @throws OptionStore.OptionDoesNotExistException An option with the given name could not be found.
+	 */
+	public void setOption(string name, object val)
+	{
+		if (!this.hasOption(name))
+		{
+			throw new OptionStore.OptionDoesNotExistException("Option \"" + name + "\" does not exist!");
+		}
+		this.options[name] = val;
+	}
+	
+	/**
+	 * Test if this option store has an option with the given name.
+	 * @param name The string name of the option to search for.
+	 * @return True if this option store has an option with the given name, false otherwise.
+	 */
+	public bool hasOption(string name)
+	{
+		return this.options.ContainsKey(name);
+	}
+	
+	/**
+	 * Return the current value of an option with the given name.
+	 * @param name The string name of the option to retreive.
+	 * @return The value of the option with the given name.
+	 * @throws OptionStore.OptionDoesNotExistException An option with the given name could not be found.
+	 */
+	public object getOption(string name)
+	{
+		if (!this.hasOption(name))
+		{
+			throw new OptionStore.OptionDoesNotExistException("Option \"" + name + "\" does not exist!");
+		}
+		return this.options[name];
+	}
+	
+	/**
+	 * Check the singleton instance to ensure there is only one.
+	 */
+	private void checkInstance()
+	{
+		if (OptionStore.Instance == null)
+		{
+			OptionStore.Instance = this;
+			GameObject.DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+	
+	/**
+	 * Calculate the path to the linked option file.
+	 */
+	private string calcPath()
+	{
+		return Path.Combine(Application.dataPath, this.FILENAME);
+	}
+	
+	/**
+	 * Save the current state of these options to the linked file.
+	 */
+	public void save()
+	{
+		FileStream file = new FileStream(this.calcPath(), FileMode.Create, FileAccess.Write, FileShare.None);
+		new BinaryFormatter().Serialize(file, this.options);
+		file.Close();
+	}
+	
+	/**
+	 * Load the options from the linked file into these options.
+	 */
+	public void load()
+	{
+		FileStream file;
+		try
+		{
+			file = new FileStream(this.calcPath(), FileMode.Open, FileAccess.Read, FileShare.Read);
+		}
+		catch (Exception e)
+		{
+			Debug.LogException(e);
+			return;
+		}
+		this.options = new BinaryFormatter().Deserialize(file) as Dictionary<string, object>;
+		file.Close();
+	}
+	
+	private void Awake()
+	{
+		this.checkInstance();
+		this.load();
+	}
+}
---
+++ Assets/Objects/UI/UI Components/Option Slider/OptionSlider.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionSlider : MonoBehaviour
+{
+	[Header("Object References")]
+	
+	[SerializeField()]
+	[Tooltip("The slider that modifies the linked option.")]
+	public Slider SLIDER;
+	
+	[SerializeField()]
+	[Tooltip("The text element that displays the minimum value.")]
+	public Text MIN_TEXT;
+	
+	[SerializeField()]
+	[Tooltip("The text element that displays the maximum value.")]
+	public Text MAX_TEXT;
+	
+	[SerializeField()]
+	[Tooltip("The text element that displays the current value.")]
+	public Text VAL_TEXT;
+	
+	[Header("Option Settings")]
+	
+	[SerializeField()]
+	[Tooltip("The name of the linked option.")]
+	public string NAME;
+	
+	[SerializeField()]
+	[Tooltip("The minimum value.")]
+	public float MIN;
+	
+	[SerializeField()]
+	[Tooltip("The maximum value.")]
+	public float MAX;
+	
+	[SerializeField()]
+	[Tooltip("The default value.")]
+	public float DEFAULT;
+	
+	/**
+	 * Check to ensure the minimum is lesser than the maximum.
+	 * @throws Exception The minimum is greater than or equal to the maximum.
+	 */
+	private void checkRange()
+	{
+		if (this.MIN >= this.MAX)
+		{
+			throw new Exception("The minimum is greater than the maximum!");
+		}
+	}
+	
+	/**
+	 * Update the minimum and maximum displays and the slider value bounds.
+	 */
+	private void updateRange()
+	{
+		this.MIN_TEXT.text = this.MIN.ToString();
+		this.MAX_TEXT.text = this.MAX.ToString();
+		this.SLIDER.minValue = this.MIN;
+		this.SLIDER.maxValue = this.MAX;
+	}
+	
+	/**
+	 * Set the value of this slider to that of the linked option or the default if not found.
+	 */
+	private void fetchValue()
+	{
+		if (!OptionStore.Instance.hasOption(this.NAME))
+		{
+			OptionStore.Instance.addOption(this.NAME, this.DEFAULT);
+		}
+		this.SLIDER.value = (float)OptionStore.Instance.getOption(this.NAME);
+		this.VAL_TEXT.text = this.SLIDER.value.ToString();
+	}
+	
+	/**
+	 * Update the value display and the linked option with the value of the linked slider.
+	 */
+	public void updateValue()
+	{
+		OptionStore.Instance.setOption(this.NAME, this.SLIDER.value);
+		this.VAL_TEXT.text = this.SLIDER.value.ToString();
+	}
+	
+	private void Awake()
+	{
+		this.checkRange();
+		this.updateRange();
+		this.fetchValue();
+	}
+}
