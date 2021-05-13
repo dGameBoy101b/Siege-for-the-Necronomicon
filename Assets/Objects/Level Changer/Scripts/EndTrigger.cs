c673ab031d7fc6e3261ceffe486b775ea80d23fa
AllanDotExe <80499423+AllanDotExe@users.noreply.github.com>
2021-05-13 11:26:43 +0800

    Transition refinements and additions.
    - Added triggers for starting the combat phase and for ending phase (shattering barrier).
    - Added audio for when the barrier shatters.
    - Added survived music
    - Added teleportation audio
    - Added Starting orb
    - Added camera shake, roar audio effect and earthquake audio effect when starting the combat phase.
    - Minor code fixes and adjustments.

---
+++ Assets/Objects/Level Changer/Scripts/EndTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author Allan Zheng 33690777
+ * @date 12/05/2021
+ * Is the trigger for the end barrier.
+ */
+public class EndTrigger : MonoBehaviour
+{
+    [SerializeField()]
+    [Tooltip("The Level Changer")]
+    public LevelChanger LEVEL_CHANGER;
+
+    [SerializeField()]
+    [Tooltip("The End Barrier")]
+    public GameObject ENDING_BARRIER;
+
+    /*
+     * Is the trigger for the end barrier.
+     */
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("End Trigger Activated");
+        Destroy(ENDING_BARRIER);
+        LEVEL_CHANGER.progress();
+    }
+}
---
+++ Assets/Objects/Level Changer/Scripts/shatter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author Allan Zheng 33690777
+ * @date 12/05/2021
+ * Handles the shattering of the end level barrier.
+ */
+
+public class shatter : MonoBehaviour
+{
+    [SerializeField()]
+    [Tooltip("The shattered variant of the end game Barrier")]
+    public GameObject SHATTERED;
+
+    [SerializeField()]
+    [Tooltip("All of the audio for when the barrier shatters")]
+    public AudioSource[] BARRIER_SHATTER;
+
+    [SerializeField()]
+    [Tooltip("The amount of power the barrier shatters with. (The higher the stronger the shatter)")]
+    public float BREAK_FORCE;
+
+    /*
+     * Shatters the end game barrier.
+     */
+    public void shatterBarrier()
+    {
+        playShatterFX();
+        GameObject frac = Instantiate(SHATTERED, transform.position, transform.rotation);
+
+        foreach (Rigidbody rb in frac.GetComponentsInChildren<Rigidbody>())
+        {
+            Vector3 force = (rb.transform.position - transform.position).normalized * BREAK_FORCE;
+            rb.AddForce(force);
+        }
+
+        Destroy(gameObject);
+    }
+
+    /*
+     * Plays all the audio associated with the shattering of the endgame barrier.
+     */
+    protected void playShatterFX()
+    {
+        foreach (AudioSource aud in this.BARRIER_SHATTER)
+        {
+            aud.Play();
+        }
+    }
+
+
+}
